8a12429d4b844e180aaf35673d5eba66bbb29870
YusufStar <07yusufstar@gmail.com>
2025-11-15 19:59:48 +0300

    exit 0

--- app/main.ts
+++ app/main.ts
@@ -6,16 +6,35 @@ const rl = createInterface({
 });
 
 const commands: {
-    description: string;
     command: string;
-}[] = []
+    description?: string;
+    args?: string[];
+    run: (args: string[]) => void | Promise<void>;
+}[] = [
+    {
+        description: 'Exit the shell with optional exit code',
+        command: 'exit',
+        args: ['code'],
+        run(args) {
+            const code = args.length > 0 ? Number(args[0]) : 0;
+            const exitCode = Number.isNaN(code) ? 1 : code;
+
+            process.exit(exitCode);
+        }
+    }
+]
 
 function ask() {
-    rl.question("$ ", (command) => {
-        const available_command = commands.find((data) => data.command === command);
-        if (!available_command) {
-            console.log(`${command}: command not found`)
+    rl.question("$ ", (input) => {
+        const [commandText, ...commandArgs] = input.split(" "); // exit 0 -> command = exit, commandArgs = [0]
+
+        const find_command = commands.filter((data) => data.command === commandText)[0]
+        if (!find_command) {
+            console.log(`${input}: command not found`)
         }
+
+        const {run, command, args, description} = find_command;
+        run(commandArgs)
 
         ask()
     });
