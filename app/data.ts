4a363cc4fa47f409b24577a30c1e51137cb3e17c
YusufStar <07yusufstar@gmail.com>
2025-11-15 20:25:42 +0300

    type command added

--- app/data.ts
+++ app/data.ts
@@ -1,6 +1,6 @@
 export const commands = [
     {
-        description: 'Exit the shell with optional exit code',
+        description: 'Terminates the shell with an optional exit code.',
         command: 'exit',
         args: [
             {
@@ -16,7 +16,7 @@ export const commands = [
         }
     },
     {
-        description: 'Print text to output',
+        description: 'Prints the provided text to the console.',
         command: 'echo',
         args: [
             {
@@ -27,6 +27,31 @@ export const commands = [
         ],
         run: async (args: string[]) => {
             console.log(args.join(" "));
+        }
+    },
+    {
+        description: 'Displays whether a command is a shell builtin or not.',
+        command: 'type',
+        args: [
+            {
+                type: 'string',
+                name: 'type',
+                size: 'one'
+            }
+        ],
+        run: async (args: string[]) => {
+            const argCommand = args[0]
+
+            if (typeof argCommand === 'string') {
+                const findCommand = commands.find(({command}) => command === argCommand);
+                if (findCommand) {
+                    console.log(`${argCommand} is a shell builtin`);
+                } else {
+                    console.log(`${argCommand}: not found`);
+                }
+            } else {
+                return;
+            }
         }
     }
 ];
